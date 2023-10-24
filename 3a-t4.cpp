863ff7c8407fb0b1c2f8cc2e4b07c0506f90ee47
leralezh <146587786+leralezh@users.noreply.github.com>
2023-10-24 21:23:52 +0300

    modify the program according to task 4.3

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,21 +1,37 @@
-#include <iostream>
+#include<iostream>
+#include<limits>
 using namespace std;
 
 int main()
 {
 int k, i = 1, sum_k = 0, number;
-cout << "Enter the amount of integers to sum = ";
-cin >> k;
-if (k == 0) {
-cout << "The sum of 0 integers is 0." << endl;
-return 0;
+cout << " Enter the amount of integers to sum = "; cin >> k;
+if(k==0)
+{
+    cout<<"0";
 }
+else{
 do {
 cout << "Enter integer nr. " << i << ": ";
 cin >> number;
+while(1)
+{
+    if(cin.fail())
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        cout<<"Warning: Input is not an integer! Please try again."<<endl;
+        cin>>number;
+    }
+    if(!cin.fail())
+    {
+       break;
+    }
+}
 sum_k += number; i++;
 } while (i <= k);
-cout << "The total sum of " << k << " integers is: " << sum_k;
+cout << " The total sum of " << k << " integers is: "  << sum_k;
+}
 return 0;
 }
 
